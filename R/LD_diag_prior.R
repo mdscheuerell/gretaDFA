e70776c09ec6ddef48e7e9d4003ff28e1b61b147
mdscheuerell <mark.scheuerell@noaa.gov>
2018-05-08 10:47:57 -0700

    added citation

--- R/LD_diag_prior.R
+++ R/LD_diag_prior.R
@@ -1,10 +1,6 @@
-## Example in Stan
-## https://gist.github.com/mbjoseph/952b807bf5aad4a72a9d865f84d67afa
-## lines 74-77:
-## // priors for diagonal entries (Leung and Drton 2016)
-## for (i in 1:k) {
-##   target += (k - i) * log(beta_diag[i]) - .5 * beta_diag[i] ^ 2 / sigma_L;
-## }
+## prior for diagonal of factor loadings matrix
+## based on eqn 2.2 in Leung and Drton (2016) 
+## https://arxiv.org/abs/1409.7672v1
 
 LD_diag <- function(x, sigma) {
   ## log pdf for Leung & Drton prior
