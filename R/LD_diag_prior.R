a5f7c3cf048a18719d9426f7e5a395f5aeac2fc2
mdscheuerell <mark.scheuerell@noaa.gov>
2018-05-08 12:01:39 -0700

    rearranged math ops

--- R/LD_diag_prior.R
+++ R/LD_diag_prior.R
@@ -4,10 +4,10 @@
 
 LD_diag <- function(x, sigma) {
   ## log pdf for Leung & Drton prior
-  ## x is diag of loadings matrix
+  ## x is diag of loadings matrix; x > 0
   ## sigma is scale parameter
   n <- length(x)
   ## log pdf
-  lpdf <- (n - seq(n)) * log(x) - (0.5/sigma*x^2)
+  lpdf <- (n - seq(n)) * log(x) - x^2 / (2 * sigma)
   return(lpdf)
 }
