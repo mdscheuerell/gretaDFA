48aa4910355b6a8069306c08819c22d8208a7e3e
mdscheuerell <mark.scheuerell@noaa.gov>
2018-05-31 16:41:13 -0700

    working version of 1D ld

--- R/LD_defn.R
+++ R/LD_defn.R
@@ -73,78 +73,17 @@ ld_distribution <- R6Class (
   )
 )
 
-z1 <- ld(i = 1, M = 4, C0 = 1, dim = 1)
-z2 <- ld(i = 2, M = 4, C0 = 1, dim = 1)
-z3 <- ld(i = 3, M = 4, C0 = 1, dim = 1)
-
-i <- model(z1, z2, z3)
-
-# plan(multicore)
-
-draws <- mcmc(i, chains = 2, n_samples = 1000, warmup = 500, thin = 4)
-plot(draws)
-
-
 
 zd <- zeros(M-1)
 for(i in 1:(M-1)) {
   
-  zd[i] <- ld(i, M = M, C0 = 1, dim = 1)
+  zd[i] <- ld(i, M = M, C0 = 0.5, dim = 1)
   
 }
 m <- model(zd)
-draws <- mcmc(m, chains = 2, n_samples = 2000, warmup = 500, thin = 4)
+draws <- mcmc(m, chains = 2, n_samples = 2500, warmup = 500, thin = 10)
 plot(draws)
 
 coda::effectiveSize(draws)
 coda::gelman.diag(draws)
 
-
-
-
-
-
-
-dd <- do.call(rbind, draws)
-## approximate CV
-apply(dd, 2, sd) / apply(dd, 2, mean)
-
-apply(dd, 2, quantile)
-dd <- apply(dd, 2, sort)
-
-
-
-coda::effectiveSize(draws)
-coda::gelman.diag(draws)
-
-
-npdf <- function(x, mu = 0, C0 = 1) {
-  pp <- exp(-(x - mu)^2 / (2*C0^2))
-  np <- pp/sqrt(2*pi*C0^2)
-  return(np)
-}
-
-xx <- seq(-500,500)/100
-plot(xx, npdf(xx), type="l")
-
-
-
-
-pld <- function(X, C0 = 1) {
-  n <- length(X)
-  pdf <- X^(n - seq(n)) * exp(- X^2 / (2 * C0))
-  return(pdf)
-}
-
-xx <- seq(1000)/100
-yy <- matrix(xx,length(xx),2)
-pp <- apply(yy, 1, pld)
-
-matplot(xx, t(apply(yy, 1, pld)), type="l")
-
-
-N <- 3
-C0 <- 1
-
-
-const * tf$log(x) - x ^ fl(2) / (fl(2) * C0)
